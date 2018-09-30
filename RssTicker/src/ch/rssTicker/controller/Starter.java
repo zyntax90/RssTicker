c94d7e2fb1af8640d65e615b2e488f523db85368
Zyntax <Zyntax@192.168.0.66>
2018-09-30 11:16:26 +0200

    Added main view and starter app

---
+++ RssTicker/src/ch/rssTicker/controller/Starter.java
@@ -0,0 +1,29 @@
+package ch.rssTicker.controller;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Starter extends Application {
+
+	public static void main(String[] args) {
+
+		Application.launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/ch/rssTicker/view/FrmRssTicker.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setTitle("RssTorrentTicker");
+		primaryStage.setScene(scene);
+		primaryStage.sizeToScene();
+		primaryStage.show();
+
+	}
+	
+
+}
